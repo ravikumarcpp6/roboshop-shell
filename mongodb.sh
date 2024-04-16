685c196474a2e57d83542f2b87d055a1f3ac5fa6
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-04-16 14:26:15 +0530

    shell script

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+    then 
+        echo "$R ERROR...please run with root user $N"
+    else
+        echo "$G You are root user $N"    
