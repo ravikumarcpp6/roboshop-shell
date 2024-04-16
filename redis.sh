d68c5ea08ce38317e95066b77cd1b5374232914a
ravikumarcpp6 <ravikumarcpp6@gmail.com>
2024-04-16 21:38:49 +0530

    set -e

---
+++ redis.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+exec &>$LOGFILE
+
+echo "The script started executing at:$TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+    then
+        echo -e "$R ERROR:: PLEASE RUN WITH ROOT USER $N"
+        exit 1
+    else
+        echo -e "$G YOU ARE A ROOT USER $N"    
+fi    
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+
+       then
+            echo -e "$2...$R FAILED $N"
+            else 
+                echo -e "$2...$G SUCCESS $N"
+    fi            
+}
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
+
+VALIDATE $? "Installing Remi Release"
+
+dnf module enable redis:remi-6.2 -y
+
+VALIDATE $? "Enabling Remis"
+
+dnf install redis -y
+
+VALIDATE $? "installing Redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+
+VALIDATE $? "Allowing Remote Connections"
+
+systemctl enable redis
+
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis
+
+VALIDATE $? "Redis started"
